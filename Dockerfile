fe793f560c4de1619943674409bcad879b490b2a
snulifeo2 <snulifeo2@naver.com>
2023-08-16 10:56:29 +0900

    test Map

--- Dockerfile
+++ Dockerfile
@@ -3,3 +3,5 @@ FROM openjdk:11-jdk
 WORKDIR /app
 
 COPY build/libs/*.jar app.jar
+
+CMD ["java", "-Dspring.profiles.active=prod", "-jar", "app.jar"]
--- src/main/java/kr/co/tj/MainController.java
+++ src/main/java/kr/co/tj/MainController.java
@@ -1,5 +1,8 @@
 package kr.co.tj;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +14,9 @@ public class MainController {
 	@GetMapping({"/", "/check"})
 	public ResponseEntity<?> test(){
 		
-		
-		return ResponseEntity.ok().body("check_test");
+		Map<String, String> map = new HashMap<>();
+		map.put("hello", "world");
+		return ResponseEntity.ok().body(map);
 		
 	}
 	
